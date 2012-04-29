062b2f47d8d84df726562e47df3459168c046962
Antonio Lagnada <antonio@lagnada.com>
2012-04-29 00:12:28 -0700

    Home Page  - added api version and api resource uri templates

--- src/main/java/com/lagnada/xmx1024/controller/HomeController.java
+++ src/main/java/com/lagnada/xmx1024/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.lagnada.xmx1024.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,9 @@ public class HomeController {
 
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
+    @Value("${application.version}")
+    private String applicationVersion;
+
     @RequestMapping(value = "/index", method = GET)
     public String home(Locale locale, Model model) {
         logger.info("Welcome home! the client locale is " + locale.toString());
@@ -24,6 +28,7 @@ public class HomeController {
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
         String formattedDate = dateFormat.format(date);
         model.addAttribute("serverTime", formattedDate);
+        model.addAttribute("version", applicationVersion);
         return "home";
     }
 
